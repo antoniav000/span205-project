a533d78e1084883242be5521b2fe9d19eb92ec0d
Antonia Vazquez <antoniav@udel.edu>
2024-12-27 20:10:49 -0500

    additional format edits

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,11 @@ console.log("Welcome to My Website!");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Mexican Art Showcase is ready!");
 });
+document.addEventListener("DOMContentLoaded", () => {
+    // Get all the images on the page
+    const images = document.querySelectorAll("img");
+    // Loop through the images and add the fade-in class
+    images.forEach((image) => {
+        image.classList.add("fade-in");
+    });
+});
